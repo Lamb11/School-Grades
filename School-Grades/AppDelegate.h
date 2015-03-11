3bb19ee4aeb9e468da9adb46cb620d29af76a5ea
Lamb11 <abc_1118@hotmail.com>
2015-03-10 18:12:04 -0600

    Initial Commit

---
+++ School-Grades/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  School-Grades
+//
+//  Created by Alberto Cordero Arellanes on 10/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ School-Grades/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  School-Grades
+//
+//  Created by Alberto Cordero Arellanes on 10/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
