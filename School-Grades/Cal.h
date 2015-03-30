5e8635b6b8968f2bf56d204a8021a476bfa6334d
Lamb11 <abc_1118@hotmail.com>
2015-03-29 18:15:40 -0600

    Agregue Visualización

---
+++ School-Grades/Cal.h
@@ -0,0 +1,13 @@
+//
+//  Cal.h
+//  School-Grades
+//
+//  Created by Alberto Cordero Arellanes on 29/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cal : UIViewController
+
+@end
---
+++ School-Grades/Cal.m
@@ -0,0 +1,37 @@
+//
+//  Cal.m
+//  School-Grades
+//
+//  Created by Alberto Cordero Arellanes on 29/03/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import "Cal.h"
+
+@interface Cal ()
+
+@end
+
+@implementation Cal
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
